2006de028b586028324d1a8e62eefcd206bc8bc6
BenM4k <bennymakomo@gmail.com>
2023-11-29 15:32:42 +0200

    Links redirection

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -5,13 +5,13 @@ import { FaGithub, FaLinkedin } from 'react-icons/fa';
 const SocialMedia = () => {
   return (
     <div className='app__social'>
-        <a href="https://twitter.com/______anomaly">
+        <a href="https://twitter.com/______anomaly" target="_blank">
             <BsTwitter />
         </a>
-        <a href="https://github.com/BenM4k">
+        <a href="https://github.com/BenM4k" target="_blank">
             <FaGithub />
         </a>
-        <a href="https://www.linkedin.com/in/benedict-makomo/">
+        <a href="https://www.linkedin.com/in/benedict-makomo/" target="_blank">
             <FaLinkedin />
         </a>
     </div>
